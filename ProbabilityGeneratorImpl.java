48c03792468e21c944ca36f99598d18b08d9b0af
shubhamkumar0 <shubham.kumar099@gmail.com>
2020-07-08 01:31:22 +0530

    added comments & binary search

--- ProbabilityGeneratorImpl.java
+++ ProbabilityGeneratorImpl.java
@@ -5,23 +5,34 @@ import java.io.IOException;
 
 import java.util.Random;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class ProbabilityGeneratorImpl implements ProbabilityGenerator{
+  //to store the sentences
   private ArrayList<String> sentences = new ArrayList<String>();
+  //to store the starting of each range of cummulative probabilities
   private ArrayList<Double> rangeStart = new ArrayList<Double>();
   private Double start=0D;
-  ProbabilityGeneratorImpl(String pathToCsv) throws Exception {
-    BufferedReader csvReader = new BufferedReader(new FileReader(pathToCsv));
 
+  ProbabilityGeneratorImpl(String pathToCsv) throws Exception {
+    /*
+    We will iterate each line and calucate the cummulative probabilities.
+    The range between current probability and current cummulative probability
+    will give us a range.
+    [0,firstSentenceProbab), [firstSentenceProbab, CummulativeProbab)...
+    This range will give us the probaility =
+    len(range)/len(complete range) .
+    The len(complete range is 1 here as probaility is between 0 to 1.
+    We store the start index of these range in the arraylist to iterate later.
+    */
+    BufferedReader csvReader =
+      new BufferedReader(new FileReader(pathToCsv));
     String row = "";
-    int index=0;
     while ((row = csvReader.readLine()) != null) {
-        String[] data = row.split(",");
-        // Double end = start+Double.parseDouble(data[1]);
-        // System.out.println(start+" , "+end);
-        sentences.add(data[0]);
-        rangeStart.add(start);
-        start+=Double.parseDouble(data[1]);
+      String[] data = row.split(",");
+      sentences.add(data[0]);
+      rangeStart.add(start);
+      start+=Double.parseDouble(data[1]);
     }
 
     if (csvReader != null) {
@@ -30,14 +41,14 @@ public class ProbabilityGeneratorImpl implements ProbabilityGenerator{
   }
 
   public String getNextString(){
+    /*
+    We are generating random real numbers between 0 and 1;
+    We find at where the number lies, and choose that same sentence.
+    */
     Random rand = new Random();
     Double rand_idx= rand.nextDouble();
     int i=0;
-    for(;i<rangeStart.size();i++){
-      if(rangeStart.get(i)>rand_idx){
-        break;
-      }
-    }
-    return sentences.get(i-1);
+    int idx = ((Collections.binarySearch(rangeStart, rand_idx))*-1)-1;
+    return sentences.get(idx-1);
   }
 }
--- app.java
+++ app.java
@@ -7,24 +7,25 @@ public class app{
 	public static void main(String[] args) {
 		// init the prob generator
 		try{
-     ProbabilityGenerator probabilityGenerator = new ProbabilityGeneratorImpl("probabilities.csv");
-		 System.out.print(probabilityGenerator);
-     //numberOfTimes : Number of times that method is called. We can have any value for the same
-     Scanner myInput = new Scanner( System.in );
-     System.out.print( "Enter numberOfTimes: " );
-     int numberOfTimes = myInput.nextInt();
-		 PrintWriter writer = new PrintWriter(new File("output.csv"));
-     for(int i=0; i<numberOfTimes; i++) {
-		 		StringBuilder sb = new StringBuilder();
-        String nextStr = probabilityGenerator.getNextString();
-        // write in the output file
+			ProbabilityGenerator probabilityGenerator =
+				new ProbabilityGeneratorImpl("probabilities.csv");
+			//numberOfTimes : Number of times that method is called.
+			//We can have any value for the same
+			Scanner myInput = new Scanner( System.in );
+			System.out.print( "Enter numberOfTimes: " );
+			int numberOfTimes = myInput.nextInt();
+			PrintWriter writer = new PrintWriter(new File("output.csv"));
+			for(int i=0; i<numberOfTimes; i++) {
+				StringBuilder sb = new StringBuilder();
+				String nextStr = probabilityGenerator.getNextString();
+				// write in the output file
 				sb.append(nextStr);
 				sb.append("\n");
 				writer.write(sb.toString());
-		 }
-		 writer.close();
-	 } catch (Exception e) {
-	    	e.printStackTrace();
-	  }
+			}
+			writer.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
